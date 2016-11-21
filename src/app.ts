9e6173b78667363a3f63595ddfde682bff7f58b9
Jason Boyll <jason.boyll@gmail.com>
2016-11-20 21:36:01 -0500

    wip

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+// Get current location
+function geoFindLocation(): void {
+    // Verify browser support
+    if (!navigator.geolocation) {
+        alert("Geolocation not supported by your browser");
+        return;
+    }
+
+
+}
+// Get weather for current location
+
+// Update display of current weather
